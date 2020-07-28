d87d36123b6076bc45a55887631eb2a18bb27abf
vishal chintham <chinthamvishal.2019@vitstudent.ac.in>
2020-07-28 10:31:57 +0530

    added the strategies for teacher and student

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const Teacher = require("./models/teacher.js");
+const Student = require("./models/student.js");
 
-app.use(express.static('public'));
+app.use(express.static(__dirname+'/public'));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+
+//setting up the passport package and express-session package
 app.use(session({
   secret:process.env.SECRET,
   resave:false,
@@ -18,12 +22,26 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/QuizDB",{useNewUrlParser:true,useUnifiedTopology: true});
+//databse setup
+mongoose.connect("mongodb://localhost/QuizDB",{useNewUrlParser:true,useUnifiedTopology: true});
 mongoose.set("useCreateIndex",true);
+
+//passportforteacher
+passport.use("teahcerLocal",Teacher.createStrategy());
+passport.serializeUser(Teacher.serializeUser());
+passport.deserializeUser(Teacher.deserializeUser());
+
+//passportforstudent
+passport.use("studentLocal",Student.createStrategy());
+passport.serializeUser(Student.serializeUser());
+passport.deserializeUser(Student.deserializeUser());
+
 
 
 ////////////////////////////////////////////////////////////////////   GETS    //////////////////////////////////////////////////////////////
-
+app.get("/",(req,res)=>{
+  res.send("landing page");
+})
 
 
 
