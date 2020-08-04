23e175ab3b7dae7b6fb687ce41def1a6a72f2cb9
vishal chintham <chinthamvishal.2019@vitstudent.ac.in>
2020-08-04 22:23:42 +0530

    resolved an error

--- app.js
+++ app.js
@@ -301,26 +301,26 @@ app.post("/te/exam/:id/students",(req,res)=>{
 
 //still the student schema must be changed and try to add a roll number in it and try to fetch the students using roll number only!!!
 app.post("/te/exam/:id/start",(req,res)=>{
-  id=req.params.id;
-  Exam.findById(id,(error,exam)=>{
-    exam.students.forEach((s)=>{
-        Student.findOne({username:s},(er,s)=>{
-          if(er) console.log(er);
-          else {
-            s.examid.push(id);
-            console.log(s);
-          }
-        });
-    });
-    exam.status="started";
-    exam.save((e,exa)=>{
-      if(e) console.log(e);
-      else {
-        console.log(exa);
-        res.send("success");
-      }
-    });
-  });
+  // id=req.params.id;
+  // Exam.findById(id,(error,exam)=>{
+  //   exam.students.forEach((s)=>{
+  //       Student.findOne({username:s},(er,s)=>{
+  //         if(er) console.log(er);
+  //         else {
+  //           s.examid.push(id);
+  //           console.log(s);
+  //         }
+  //       });
+  //   });
+  //   exam.status="started";
+  //   exam.save((e,exa)=>{
+  //     if(e) console.log(e);
+  //     else {
+  //       console.log(exa);
+  //       res.send("success");
+  //     }
+  //   });
+  // });
 })
 
 
@@ -382,6 +382,7 @@ function run(){
       console.log(exams[i].date.getTime()<=obj.getTime() && (exams[i].date.getDate()<=obj.getDate() && exams[i].date.getMonth()<=obj.getMonth() && exams[i].date.getYear()<=obj.getYear()));
       if(exams[i].date.getTime()<=obj.getTime()&& (exams[i].date.getDate()<=obj.getDate() && exams[i].date.getMonth()<=obj.getMonth() && exams[i].date.getYear()<=obj.getYear())){
         var a= axios.post("http://localhost:3000/te/exam/"+exams[i]._id+"/start");
+        exams.splice(i,1);
       }
     }
   },1000);
